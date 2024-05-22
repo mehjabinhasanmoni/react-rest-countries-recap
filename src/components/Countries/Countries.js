c1cc8e4a313b5c54f1b194600dd1ccb305fb7942
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2024-05-22 10:59:19 +0600

    Advanced Data pass

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -12,9 +13,11 @@ const Countries = () => {
   return (
     <div>
       <h1>Total Countries - {countries.length}</h1>
-      {countries.map((country) => (
-        <Country country={country}></Country>
-      ))}
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country country={country} key={country.cca3}></Country>
+        ))}
+      </div>
     </div>
   );
 };
